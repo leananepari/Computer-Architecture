4a0bc5d5e16352438ae6aae917580a4735b6186b
Leana <leananeparidze.com>
2020-01-28 07:18:56 -0800

    completes cpu run read and write

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,7 +7,9 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.ram = [0] * 256
+        self.reg = [0] * 8
+        self.pc = 0
 
     def load(self):
         """Load a program into memory."""
@@ -59,7 +61,36 @@ class CPU:
             print(" %02X" % self.reg[i], end='')
 
         print()
+    
+    def ram_read(self, address):
+      return self.ram[address]
+      
+    def ram_write(self, value, address):
+      self.ram[address] = value
 
     def run(self):
         """Run the CPU."""
-        pass
+        # IR = self.ram[self.pc]
+        while True:
+          IR = self.ram[self.pc]
+          operand_a = self.ram_read(self.pc + 1)
+          operand_b = self.ram_read(self.pc + 2)
+          if IR == 0b10000010: #LDI
+            self.reg[operand_a] = operand_b
+            self.pc += 3
+          elif IR == 0b01000111: #PRN
+            print(self.reg[operand_a])
+            self.pc += 2
+          elif IR == 0b00000001: #HLT
+            break
+          else:
+            print(f"Error: Unknown command: {IR}")
+            break
+          
+
+cpu = CPU()
+
+cpu.load()
+cpu.trace()
+cpu.run()
+
